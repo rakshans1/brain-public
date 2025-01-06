27e0382c08d63338499702a091801332c323bb75
Rakshan Shetty <shetty.raxx555@gmail.com>
2025-01-06 11:45:58 +0530

    Update content

--- quartz/plugins/emitters/tagPage.tsx
+++ quartz/plugins/emitters/tagPage.tsx
@@ -104,7 +104,7 @@ export const TagPage: QuartzEmitterPlugin<Partial<TagPageOptions>> = (userOpts) 
         if (slug.startsWith("tags/")) {
           const tag = slug.slice("tags/".length)
           if (tags.has(tag)) {
-if (file.data.frontmatter?.title === tag) {
+            if (file.data.frontmatter?.title === tag) {
               file.data.frontmatter.title = `${i18n(cfg.locale).pages.tagContent.tag}: ${tag}`
             }
           }
