08ee6634e1a016b47d873d68c71de340055957b5
Rakshan Shetty <shetty.raxx555@gmail.com>
2025-10-12 12:23:13 +0530

    .

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -1,8 +1,8 @@
-import { PageLayout, SharedLayout } from "./quartz/cfg"
-import * as Component from "./quartz/components"
-import { notesFilter } from "./quartz-custom/utils/filter"
-import { topicFilter } from "./quartz-custom/utils/filter"
-import * as CustomComponent from "./quartz-custom/components"
+import { PageLayout, SharedLayout } from "./quartz/cfg";
+import * as Component from "./quartz/components";
+import { notesFilter } from "./quartz-custom/utils/filter";
+import { topicFilter } from "./quartz-custom/utils/filter";
+import * as CustomComponent from "./quartz-custom/components";
 
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
@@ -18,7 +18,7 @@ export const sharedPageComponents: SharedLayout = {
       Email: "mailto:shetty.raxx555@gmail.com",
     },
   }),
-}
+};
 
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
@@ -33,25 +33,36 @@ export const defaultContentPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    Component.DesktopOnly(Component.Explorer({ title: "Topics", filterFn: topicFilter, folderClickBehavior: "collapse" })),
+    Component.DesktopOnly(
+      Component.Explorer({ title: "Topics", filterFn: topicFilter }),
+    ),
   ],
   right: [
     Component.DesktopOnly(Component.Graph()),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
-    Component.RecentNotes({ limit: 5, showTags: false, filter: notesFilter })
+    Component.RecentNotes({ limit: 5, showTags: false, filter: notesFilter }),
   ],
-}
+};
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(), CustomComponent.ArticleTitle(), CustomComponent.ContentMeta()],
+  beforeBody: [
+    Component.Breadcrumbs(),
+    CustomComponent.ArticleTitle(),
+    CustomComponent.ContentMeta(),
+  ],
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    Component.DesktopOnly(Component.Explorer({ title: "Topics", filterFn: topicFilter, folderClickBehavior: "collapse" })),
+    Component.DesktopOnly(
+      Component.Explorer({
+        title: "Topics",
+        filterFn: topicFilter,
+      }),
+    ),
   ],
   right: [],
-}
+};
